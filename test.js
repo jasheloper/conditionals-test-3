ee087bdef7a4e7b5c2815856fcf54c8c1b10cdb6
jasheloper <48870048+jasheloper@users.noreply.github.com>
2023-10-23 17:39:19 -0700

    it works

---
+++ test.js
@@ -0,0 +1,6 @@
+if (machineActive === true) {
+    machineResult === "Machine Status = ON";
+    pwd === "cheese" ? pwdResult = "Login attempt was successful!" : pwdResult = "Login attempt was unsuccessful. Try again."
+} else if (machineActive === false) {
+    machineResult === "Machine Status = OFF";
+}
